e9e7b9eb8f4388b144cfb8190dd4204594aed453
alejograjal <alejandro.grajal.s@gmail.com>
2025-05-28 19:30:27 -0600

    fix: remove extra usings

--- api/BeYou.Domain/Core/Models/BaseEntity.cs
+++ api/BeYou.Domain/Core/Models/BaseEntity.cs
@@ -1,7 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace BeYou.Domain.Core.Models;
+﻿namespace BeYou.Domain.Core.Models;
 
 public class BaseEntity : BaseSimpleDto
 {
