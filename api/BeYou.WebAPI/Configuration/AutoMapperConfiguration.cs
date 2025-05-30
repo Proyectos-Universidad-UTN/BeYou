2740df54f906f2ef7cfe60aa213d1b9ae0eb9218
BrayanAA <boaguirrea@est.utn.ac.cr>
2025-05-29 20:19:05 -0600

    feat: added  AutoMapperConfiguration class

---
+++ api/BeYou.WebAPI/Configuration/AutoMapperConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using BeYou.Infraestructure.Mappings.Mapper;
+
+namespace BeYou.WebAPI.Configuration;
+
+/// <summary>
+/// Configuration extension for auto mapper
+/// </summary>
+public static class AutoMapperConfiguration
+{
+    /// <summary>
+    /// Extension method to configure auto mapper
+    /// </summary>
+    /// <param name="services">Collection of services to use as extension</param>
+    public static void ConfigureAutoMapper(this IServiceCollection services)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+
+        services.AddAutoMapper(typeof(ModelToDtoApplicationProfile));
+        services.AddAutoMapper(typeof(DtoToModelApplicationProfile));
+    }
+}
