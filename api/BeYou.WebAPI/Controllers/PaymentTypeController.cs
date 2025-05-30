1392d4f9c68d14947c7922310858914062ddbc34
BrayanAA <boaguirrea@est.utn.ac.cr>
2025-05-29 21:43:35 -0600

    feat: added PaymentTypeController controller

---
+++ api/BeYou.WebAPI/Controllers/PaymentTypeController.cs
@@ -0,0 +1,31 @@
+﻿using Asp.Versioning;
+using BeYou.Application.Dtos.Response;
+using BeYou.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeYou.WebAPI.Controllers;
+
+/// <summary>
+/// Controller in charge of types of payment calls
+/// </summary>
+[ApiController]
+[BeYouAuthorize]
+[ApiVersion("1.0")]
+[Route("api/[controller]")]
+[Authorize(Policy = "BeYou")]
+public class PaymentTypeController(IServicePaymentType servicePaymentType) : ControllerBase
+{
+    /// <summary>
+    /// Get list of all payment types
+    /// </summary>
+    /// <returns>IActionResult</returns>
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ICollection<ResponsePaymentTypeDto>))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ErrorDetailsBeYou))]
+    public async Task<IActionResult> ListAllAsync()
+    {
+        var paymentType = await servicePaymentType.ListAllAsync();
+        return StatusCode(StatusCodes.Status200OK, paymentType);
+    }
+}
