398d177dea5f408c400e49c4361bdb745f1fb2d5
BrayanAA <boaguirrea@est.utn.ac.cr>
2025-05-29 20:20:09 -0600

    feat: added BaseReservationAuthorizeAttribute class

---
+++ api/BeYou.WebAPI/Configuration/BaseReservationAuthorizeAttribute.cs
@@ -0,0 +1,28 @@
+﻿using BeYou.Application.Enums;
+using BeYou.Utils;
+using Microsoft.AspNetCore.Authorization;
+
+namespace BeYou.WebAPI.Configuration;
+
+/// <summary>
+/// Authorization attribute for controllers
+/// </summary>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+public class BaseReservationAuthorizeAttribute : AuthorizeAttribute
+{
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    /// <returns></returns>
+    public BaseReservationAuthorizeAttribute() : base() { }
+
+    /// <summary>
+    /// Overload constructor to pass list of roles
+    /// </summary>
+    /// <param name="roles">List of roles</param>
+    public BaseReservationAuthorizeAttribute(params RoleApplication[] roles)
+    {
+        var allowedRolesAsStrings = roles.Select(x => StringExtension.Capitalize(Enum.GetName(typeof(RoleApplication), x)!));
+        Roles = string.Join(",", allowedRolesAsStrings);
+    }
+}
