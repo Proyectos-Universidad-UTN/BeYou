4b5138e91132e916bd0a27d1c19d1703561dce55
BrayanAA <boaguirrea@est.utn.ac.cr>
2025-07-01 14:10:51 -0600

    feat: update sidebar styles

--- admin-site/src/components/Sidebar/DesktopSidebar.tsx
+++ admin-site/src/components/Sidebar/DesktopSidebar.tsx
@@ -3,19 +3,23 @@
 import { List } from "@mui/material";
 import { MenuItem } from "./Types";
 import { renderMenuItem } from "./RenderMenuItem";
+import { useSidebarStore } from "@/stores/useSidebarStore"; 
 
 interface DesktopSidebarProps {
   menuItems: MenuItem[];
 }
 
 export default function DesktopSidebar({ menuItems }: DesktopSidebarProps) {
-  const header = (
-    <div className="font-bold text-xl text-center py-4 text-yellow-700 bg-pink-300 tracking-wide border-b" />
-  );
+  const { isExpanded, setIsExpanded } = useSidebarStore(); 
 
   return (
-    <div className="fixed h-full shadow w-64 bg-pink-100">
-      {header}
+    <div
+      onMouseEnter={() => setIsExpanded(true)}
+      onMouseLeave={() => setIsExpanded(false)}
+      className={`fixed top-0 left-0 h-full z-50 transition-all duration-300 ${
+        isExpanded ? "w-64" : "w-16"
+      } bg-pink-200`}
+    >
       <List>{menuItems.map((item) => renderMenuItem(item, false))}</List>
     </div>
   );
