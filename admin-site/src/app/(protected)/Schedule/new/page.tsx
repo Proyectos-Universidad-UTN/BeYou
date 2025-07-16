a5c820af44dcfa4d37cc7549a70758a6d552cd86
BrayanAA <boaguirrea@est.utn.ac.cr>
2025-07-16 09:15:02 -0600

    feat: created and modificate sidebar and schedele

--- admin-site/src/app/(protected)/Schedule/new/page.tsx
+++ admin-site/src/app/(protected)/Schedule/new/page.tsx
@@ -3,8 +3,11 @@
 import { useState } from "react";
 import { Page } from "@/components/Shared/Page";
 import { PageHeader } from "@/components/Shared/PageHeader";
-import { ScheduleFormComponent } from "../components/ScheduleForm";
-import { ScheduleDefaultValues, ScheduleForm } from "../components/ScheduleSchema";
+import { ScheduleFormComponent } from "../../(protected)/Branch/Schedule/components/ScheduleForm";
+import {
+  ScheduleDefaultValues,
+  ScheduleForm,
+} from "../../(protected)/Branch/Schedule/components/ScheduleSchema";
 import { UseMutationCallbacks } from "@/hooks/UseMutationCallbacks";
 import { UsePostSchedule } from "@/hooks/api-beyou/schedule/UsePostSchedule";
 
@@ -14,17 +17,21 @@ const NewSchedulePage = () => {
   const closeLoading = () => setLoading(false);
 
   const { mutate: postSchedule } = UsePostSchedule(
-    UseMutationCallbacks("Horario creado exitosamente", "/Branch/Schedule", closeLoading)
+    UseMutationCallbacks(
+      "Horario creado exitosamente",
+      "/Branch/Schedule",
+      closeLoading
+    )
   );
 
   const handleSubmit = (data: ScheduleForm) => {
-  setLoading(true);
-  postSchedule({
-    day: data.day as 1 | 2 | 3 | 4 | 5 | 6 | 7,
-    startHour: data.startHour,
-    endHour: data.endHour,
-  });
-};
+    setLoading(true);
+    postSchedule({
+      day: data.day as 1 | 2 | 3 | 4 | 5 | 6 | 7,
+      startHour: data.startHour,
+      endHour: data.endHour,
+    });
+  };
 
   return (
     <Page
