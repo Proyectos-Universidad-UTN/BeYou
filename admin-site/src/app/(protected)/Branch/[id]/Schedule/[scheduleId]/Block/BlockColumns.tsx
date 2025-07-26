f4c4d40a349a522a7db687bcb84587f02192bda3
BrayanAA <boaguirrea@est.utn.ac.cr>
2025-07-26 16:57:16 -0600

    feat: edit block

--- admin-site/src/app/(protected)/Branch/[id]/Schedule/[scheduleId]/Block/BlockColumns.tsx
+++ admin-site/src/app/(protected)/Branch/[id]/Schedule/[scheduleId]/Block/BlockColumns.tsx
@@ -1,5 +1,7 @@
-import { GridColDef } from "@mui/x-data-grid";
-import { BlockForm } from "./components/BlockSchema";
+import { GridColDef, GridRenderCellParams } from "@mui/x-data-grid";
+
+import { BranchScheduleBlock } from "@/types/api-beyou"; // Ajusta si es otro tipo
+import { MenuActionsBlock } from "./MenuActionsBlock";
 
 const BlockColumns: GridColDef[] = [
   { field: "id", headerName: "ID", flex: 0.5, minWidth: 100 },
@@ -14,6 +16,16 @@ const BlockColumns: GridColDef[] = [
     headerName: "Hora de fin",
     flex: 0.5,
     minWidth: 100,
+  },
+  {
+    field: "actions",
+    headerName: "Acciones",
+    sortable: false,
+    filterable: false,
+    width: 120,
+    renderCell: (params: GridRenderCellParams<BranchScheduleBlock>) => {
+      return <MenuActionsBlock id={params.row.id!} />;
+    },
   },
 ];
 
